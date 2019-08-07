6a53e57aa8beee04b1bf411aa916beb0f679f010
Blake Bishop <blake.bishop@kuali.co>
2019-08-06 17:46:31 -0700

    Playing around with the idea of a portfolio

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,30 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { css } from '@emotion/core'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+const linkStyles = css`
+  text-decoration: none;
+  color: #C3C3C3;
+`
+
+function Header ({ siteTitle = '' }) {
+  return (
+    <header>
+      <h1>
         <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+          to='/'
+          css={linkStyles}
         >
           {siteTitle}
         </Link>
       </h1>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,30 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+import { Global, css } from '@emotion/core'
+import styled from '@emotion/styled'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
 
-const Layout = ({ children }) => {
+const Main = styled.main`
+  background-color: 'black';
+`
+
+const Wrapper = styled.div`
+  padding: 1rem 3rem;
+`
+
+const globalStyles = css`
+  body {
+    background: #232323;
+    color: #F2F2F2;
+  }
+`
+
+function Layout ({ children }) {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -24,24 +40,11 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <Wrapper>
+      <Global styles={globalStyles} />
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+      <Main>{children}</Main>
+    </Wrapper>
   )
 }
 
